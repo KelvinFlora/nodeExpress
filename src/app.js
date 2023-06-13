890345262a2181df7724162c9701126f7ab454e4
Kelvin Flora <kelvin.flora@gmail.com>
2023-06-13 12:32:46 -0300

    mongoose crud

--- src/app.js
+++ src/app.js
@@ -1,48 +1,58 @@
 import express from "express";
+import db from "./config/dbConnect.js";
+import Foods from "./models/Food.js";
+
+db.on(
+  "error",
+  console.log.bind(console, "Não foi possível conectar ao banco de dados!")
+);
+db.once("open", () => {
+  console.log("Conexão com o banco de dados feita com sucesso!");
+});
 
 const app = express();
 
 app.use(express.json());
 
-const comidas = [
-  { id: 1, titulo: "Parmegiana" },
-  { id: 2, titulo: "Picanha" },
-];
-
 app.get("/", (request, response) => {
   response.status(200).send("Curso de Node");
 });
 
-app.get("/comidas", (request, response) => {
-  response.status(200).json(comidas);
+app.get("/comidas", async (request, response) => {
+  const foods = await Foods.find();
+  response.status(200).json(foods);
 });
 
-app.get("/comidas/:id", (request, response) => {
-  let index = findFood(request.params.id);
-  comidas[index].titulo = request.body.titulo;
-  response.status(200).json(comidas[index]);
+app.get("/comidas/:id", async (request, response) => {
+  const id = request.params.id;
+
+  const result = await Foods.findById(id);
+
+  response.status(201).json(result);
 });
 
-app.post("/comidas", (request, response) => {
-  comidas.push(request.body);
-  response.status(201).send("A comida foi cadastrada com sucesso!");
+app.post("/comidas", async (request, response) => {
+  const data = request.body;
+
+  const result = await Foods.create(data);
+
+  response.status(201).json(result);
 });
 
-app.put("/comidas/:id", (request, response) => {
-  let index = findFood(request.params.id);
-  comidas[index].titulo = request.body.titulo;
-  response.status(200).json(comidas);
+app.put("/comidas/:id", async (request, response) => {
+  const id = request.params.id;
+  const data = request.body;
+
+  const result = await Foods.updateOne({ _id: id }, data);
+
+  response.status(201).json(result);
 });
 
-app.delete("/comidas/:id", (request, response) => {
-  let { id } = request.params;
-  let index = findFood(id);
-  comidas.splice(index, 1);
-  response.status(200).send(`A comida ${id} foi removida com sucesso!`);
-});
+app.delete("/comidas/:id", async (request, response) => {
+  const id = request.params.id;
 
-function findFood(id) {
-  return comidas.findIndex((comida) => comida.id == id);
-}
+  const result = await Foods.deleteOne({ _id: id });
+  response.status(200).json(result);
+});
 
 export default app;
---
+++ src/models/Food.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const foodSchema = new mongoose.Schema({
+  foodName: { type: String, required: true },
+  ingredients: { type: String, required: true },
+  quantity: { type: Number, require: true },
+  size: { type: String, required: true },
+  table: { type: String, required: true },
+});
+
+const foods = mongoose.model("comidas", foodSchema);
+
+export default foods;

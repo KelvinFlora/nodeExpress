874031ba434149b477fbb131c50af4baf103ebd7
Kelvin Flora <kelvin.flora@gmail.com>
2023-06-14 14:04:46 -0300

    crud deleted from app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import Foods from "./models/Food.js";
+import routes from "./routes/index.js";
 
 db.on(
   "error",
@@ -14,45 +15,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.status(200).send("Curso de Node");
-});
-
-app.get("/comidas", async (request, response) => {
-  const foods = await Foods.find();
-  response.status(200).json(foods);
-});
-
-app.get("/comidas/:id", async (request, response) => {
-  const id = request.params.id;
-
-  const result = await Foods.findById(id);
-
-  response.status(201).json(result);
-});
-
-app.post("/comidas", async (request, response) => {
-  const data = request.body;
-
-  const result = await Foods.create(data);
-
-  response.status(201).json(result);
-});
-
-app.put("/comidas/:id", async (request, response) => {
-  const id = request.params.id;
-  const data = request.body;
-
-  const result = await Foods.updateOne({ _id: id }, data);
-
-  response.status(201).json(result);
-});
-
-app.delete("/comidas/:id", async (request, response) => {
-  const id = request.params.id;
-
-  const result = await Foods.deleteOne({ _id: id });
-  response.status(200).json(result);
-});
+routes(app);
 
 export default app;
